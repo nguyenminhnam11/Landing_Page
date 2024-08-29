4e344363363069002f5c9b5affaf2f8f06f3df5e
Nam Nguyen <nguyenminhnam1739@gmail.com>
2024-08-29 15:26:11 +0700

    save feature 20 percents

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Sidebar
 const sideBar = document.querySelector('.sidebar')
 const overlay = document.getElementById('overlay')
 const menuLinks = document.querySelectorAll('.sidebar .menu-mobi')
@@ -14,4 +15,21 @@ function hideMenu() {
 
 menuLinks.forEach(link => {
     link.addEventListener('click', hideMenu)
+})
+
+// Switch slide feedback
+const slide1 = document.querySelector('.slide-item-1')
+const slide2 = document.querySelector('.slide-item-2')
+const btnSwitch = document.querySelectorAll('.switch-slide')
+btnSwitch.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (slide1.style.transform == `translateX(500px)`) {
+            slide1.style.transform = `translateX(0)`
+            slide2.style.transform = `translateX(0)`
+        } else {
+            slide1.style.transform = `translateX(500px)`
+            slide2.style.transform = `translateX(-500px)`
+        }
+    
+    })
 })
